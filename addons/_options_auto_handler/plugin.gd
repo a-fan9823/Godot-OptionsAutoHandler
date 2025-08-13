89e14e7d2fc15614d25747f60e67c2714260b423
Walmart Wan <afanfromwalmart@gmail.com>
2025-08-12 17:12:44 -0700

    forgot to delete old files
    my bad

--- addons/_options_auto_handler/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enable_plugin():
-	add_autoload_singleton("OptionsAutoHandler", "res://addons/options_auto_handler/main.gd")
-
-func _disable_plugin():
-	remove_autoload_singleton("OptionsAutoHandler")
