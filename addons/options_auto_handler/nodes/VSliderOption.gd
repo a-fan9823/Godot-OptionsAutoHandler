9292975852665788efb670558c7353482754296a
Walmart Wan <afanfromwalmart@gmail.com>
2025-08-28 12:31:34 -0700

    I'm kinda dumb
    sliders should actually support floats now....

--- addons/options_auto_handler/nodes/VSliderOption.gd
+++ addons/options_auto_handler/nodes/VSliderOption.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 		OptionsAutoHandler.Init_Option(Option_Key.key,Option_Key.default_value)
 		var data = OptionsAutoHandler.Fetch_Option(Option_Key.key)
 		if data.status == OptionsAutoHandler.ReturnCodes.OK:
-			value = int(data.data)
+			value = float(data.data)
 		connect("value_changed",_value_changed)
 
 func _value_changed(value:float) -> void:
